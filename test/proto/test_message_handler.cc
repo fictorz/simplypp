a2e505719a58916d3fd9cbb3a385c1f54584b97e
FictoR <felix.clap@gmail.com>
2025-06-17 21:01:47 -0400

    remove unwanted comment

--- test/proto/test_message_handler.cc
+++ test/proto/test_message_handler.cc
@@ -7,9 +7,6 @@
 
 using namespace std;
 
-// Next idea is to type erase the Drawer and specify which drawer we want to
-// instantiate ie: Drawer drawer = std::make_unique<OPenGL>();
-// drawer.drawAllShapes(shapes);
 TEST(TestMessageHandler, basic) {
   proto::MyMessageHandler messageHandler;
   ASSERT_EQ(messageHandler.getName(), "test");
